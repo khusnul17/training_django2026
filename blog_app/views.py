3573e828de32adf1eb786d9e8fc9ae95370a2b25
khusnul <chusnul.sari@gmail.com>
2026-04-25 21:08:43 +0700

    fixing csrf

--- blog_app/views.py
+++ blog_app/views.py
@@ -4,15 +4,14 @@ from django.http import HttpResponse
 from django.views.decorators.csrf import ensure_csrf_cookie
 
 # Create your views here.
-def blog_list (requests):
+def blog_list (request):
     templates = loader.get_template("blog_list.html")
     return HttpResponse(templates.render())
 @ensure_csrf_cookie
-def create_blog (requests):
-    templates = loader.get_template("create_blog.html")
-    return HttpResponse(templates.render())
+def create_blog (request):
+    return render(request, "create_blog.html")
 
-def create_blog_action(requests):
-    print(requests.POST)
+def create_blog_action(request):
+    print(request.POST)
     templates = loader.get_template("blog_list.html")
     return HttpResponse(templates.render())
